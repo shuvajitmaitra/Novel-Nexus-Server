ed67abea465c623aab35df87a552cdc0e160ebd9
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-11-08 05:15:16 +0600

    Enveiroment variable setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,9 +8,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log();
+console.log();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://NovelNexusDB:dSCNhINSV4CuS6XL@cluster0.wyy6auz.mongodb.net/?retryWrites=true&w=majority";
+// ${process.env.DB_USER}
+// ${process.env.DB_PASS}
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wyy6auz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
